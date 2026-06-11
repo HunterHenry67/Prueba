feb45d536f75e50487b27ab6b523c062e9cee8ec
HunterHenry67 <enrique.madrid262804@potros.itson.edu.mx>
2026-06-10 19:51:30 -0700

    Prueba Commit

---
+++ PruebaGitHub/src/main/java/enriquemadridalvarez/pruebagithub/PruebaGitHub.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package enriquemadridalvarez.pruebagithub;
+
+/**
+ *
+ * @author BALAMRUSH
+ */
+public class PruebaGitHub {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
