04a5c5bb859b64b6a7d196797006d222572f4cf0
Andreiy Montoya Navarro <andreiymn02@gmail.com>
2026-06-10 20:23:51 -0700

    Creacion clase enrique
    Julian en peru

---
+++ PruebaGitHub/src/main/java/enriquemadridalvarez/pruebagithub/Enrique.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package enriquemadridalvarez.pruebagithub;
+
+/**
+ *
+ * @author Andre
+ */
+public class Enrique {
+    String nombre = "asado";
+    int edad;
+
+    public Enrique(int edad) {
+        this.edad = edad;
+    }
+    
+    
+}
